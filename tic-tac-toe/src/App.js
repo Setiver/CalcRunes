03e494c47f937477e7247cae9145c2190596559b
Lukasz <lukasjaniak44@gmail.com>
2023-04-28 13:12:56 +0200

    react

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Square from './res/Square';
+
+function App() {
+  return (
+    <div className="App">
+      <Square />
+      <Square />
+    </div>
+  );
+}
+
+export default App;
